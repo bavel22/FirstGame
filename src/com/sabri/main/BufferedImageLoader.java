835d9a2aed90f2a79df2f4c6d7c263e165b9a669
Bavel22 <sabrig@email.arizona.edu>
2019-05-18 12:16:55 -0700

    attempted to add BufferedImages

---
+++ src/com/sabri/main/BufferedImageLoader.java
@@ -0,0 +1,22 @@
+/* package com.sabri.main;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.imageio.ImageIO;
+
+public class BufferedImageLoader {
+
+    BufferedImage image;
+
+    public BufferedImage getImage(String path) {
+
+        try {
+            image.(getClass().getResource(path));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return image;
+    }
+}
+*/
